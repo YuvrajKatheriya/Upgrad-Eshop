b30cd3b01bc7f2811b31f26c788f55ac05695925
Yuvraj <ys050018@gmail.com>
2023-06-04 22:16:55 +0530

    project done

---
+++ capstone-backend-master/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require('./index');
+const PORT = 3001;
+
+const connectionString =
+  "mongodb+srv://yuvraj001331:yuvraj001331@cluster0.uajibtw.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+const app = express();
+
+const corsOptions = {
+  exposedHeaders: ["x-auth-token", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', routes);
+mongoose
+  .connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=> app.listen(PORT, ()=> console.log(`server is running at PORT: ${PORT}`)))
+  .catch((err) => console.log(err.message)); 
+
